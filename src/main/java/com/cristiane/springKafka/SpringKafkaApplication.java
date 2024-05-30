f4acb05d0ea286df1a7195671f219f75de6608fe
Cristiane Ferreira <cris.mayara@gmail.com>
2024-05-30 18:32:04 -0300

    :tada: init: initial commit

---
+++ src/main/java/com/cristiane/springKafka/SpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.cristiane.springKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaApplication.class, args);
+	}
+
+}
